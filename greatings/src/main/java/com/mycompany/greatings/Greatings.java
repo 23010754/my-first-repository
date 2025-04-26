bf36e8833ef698727ad6adef4d5135161693179a
23010754 vhuhwavho netshitangani <23010754@mvula.univen.ac.za>
2025-04-26 13:41:57 +0200

    first greating

---
+++ greatings/src/main/java/com/mycompany/greatings/Greatings.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.greatings;
+
+/**
+ *
+ * @author admin
+ */
+public class Greatings {
+
+    public static void main(String[] args) {
+        System.out.println(" hee ndaa nda khou humbela u dzhena!");
+    }
+}
